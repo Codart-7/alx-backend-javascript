379873383de8c075e92434204ddb5c9024a619e2
Codart-7 <iconmoa@gmail.com>
2022-12-19 13:25:10 +0100

    [update] All files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let newArray = []
+    for (const element of array) {
+      newArray.push(appendString + value);
+    }
+  
+    return newArray;
+  }
